000b682f61495247ed14029c1bb1fa4b43d68615
Adith <adithyancb3@gmail.com>
2024-06-03 13:02:11 +0530

    Made the code cleaner than previous and fixed product,index.html

--- products/admin.py
+++ products/admin.py
@@ -1,15 +1,8 @@
 from csv import list_dialects
 from django.contrib import admin
-
-# Register your models here.
-
 from .models import *
-
-
-
-
+# Register your models here.
 admin.site.register(Category)
-
 class ProductImageAdmin(admin.StackedInline):
     model =ProductImage
 
@@ -26,11 +19,7 @@ class ColorVariantAdmin(admin.ModelAdmin):
 @admin.register(SizeVariant)
 class SizeVariantAdmin(admin.ModelAdmin):
     list_display = ['size_name' , 'price']
-
     model = SizeVariant
 
-
 admin.site.register(Product ,ProductAdmin)
-
-
 admin.site.register(ProductImage)
--- products/views.py
+++ products/views.py
@@ -1,14 +1,9 @@
-from pydoc import render_doc
 from django.shortcuts import render
 from products.models import Product
-
-
-
 
 def get_product(request , slug):
     try:
         product = Product.objects.get(slug =slug)
         return render(request  , 'product/product.html' , context = {'product' : product})
-
     except Exception as e:
         print(e)
